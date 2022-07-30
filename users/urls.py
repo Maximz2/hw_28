b080326c4ef4d7c8cf09787cbae999d94718f836
Maxim <99118327+Maximz2@users.noreply.github.com>
2022-07-30 19:25:09 +0300

    Change to path('<int:pk>/delete/', views.UserDeleteView.as_view()),

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path('user/', views.UserView.as_view()),
-    path('user/<int:pk>', views.UserDetailView.as_view()),
-    path('user/create/', views.UserCreateView.as_view()),
-    path('user/<int:pk>/update/', views.UserUpdateView.as_view()),
+    path('/', views.UserView.as_view()),
+    path('<int:pk>', views.UserDetailView.as_view()),
+    path('create/', views.UserCreateView.as_view()),
+    path('<int:pk>/update/', views.UserUpdateView.as_view()),
     path('<int:pk>/delete/', views.UserDeleteView.as_view()),
    ]
